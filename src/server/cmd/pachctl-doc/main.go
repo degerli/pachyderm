aeb1ecaa1dec51a20ac8f4b03e55799c45357da9
Yusuf Simonson <simonson@gmail.com>
2019-01-11 15:51:56 -0800

    Replace .md links with .html in cobra generated docs

--- src/server/cmd/pachctl-doc/main.go
+++ src/server/cmd/pachctl-doc/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"regexp"
+
 	"github.com/pachyderm/pachyderm/src/server/cmd/pachctl/cmd"
 	"github.com/pachyderm/pachyderm/src/server/pkg/cmdutil"
 
 	"github.com/spf13/cobra/doc"
 )
+
+var MD_LINK_PATTERN = regexp.MustCompile(".md$")
 
 type appEnv struct{}
 
@@ -13,10 +17,19 @@ func main() {
 	cmdutil.Main(do, &appEnv{})
 }
 
+func filePrepender(value string) string {
+	// No prepending needs to be done
+	return value
+}
+
+func linkHandler(value string) string {
+	return MD_LINK_PATTERN.ReplaceAllLiteralString(value, ".html")
+}
+
 func do(appEnvObj interface{}) error {
 	rootCmd, err := cmd.PachctlCmd()
 	if err != nil {
 		return err
 	}
-	return doc.GenMarkdownTree(rootCmd, "./doc/pachctl/")
+	return doc.GenMarkdownTreeCustom(rootCmd, "./doc/pachctl/", filePrepender, linkHandler)
 }
